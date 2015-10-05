8899b78f93f30cee05d6e755e1c7308694ac6e55
Deathscythe84 <alanchuntington@hotmail.com>
2015-10-05 22:27:00 +0100

    Automatic Login now added

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Register.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Register.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
